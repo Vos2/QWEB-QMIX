d758947e40a510673aae020ac75b7820d74e4e52
Parkuman <parkumania@gmail.com>
2020-06-01 14:45:33 -0400

    adds functional, responsive testimonial carousel

--- qmix/src/javascript/home.js
+++ qmix/src/javascript/home.js
@@ -1,8 +1,9 @@
 // ___Functions___
 
 // Initializes the page with starter functions
-function __init__(){
+function __init__() {
     navDefault();
+    createTestimonialCarousel();
     state = Number(getCookie("nav"));
     switchFocus(state);
 }
@@ -15,7 +16,7 @@ function setCookie(name, value, days) {
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
-    
+
     document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 
@@ -42,29 +43,29 @@ function mobileNav() {
 }
 
 // Changes the navigation bar focus to the last on clicked
-function switchFocus(state){
+function switchFocus(state) {
     var focus = ['_home', '_events', '_alumini', '_contact'];
 
-    for (i = 0; i < 4; i++){
+    for (i = 0; i < 4; i++) {
         $("#" + focus[i]).attr('style', "");
     }
     $("#" + focus[state]).attr('style', "color: wheat !important; font-weight: bold !important;");
 }
 
 // Changes the navigation bar to become darker as the user scrolls down the page
-function scrollColour(){
+function scrollColour() {
     var s = $(window).scrollTop(),
-            d = $(document).height(),
-            c = $(window).height();
-            scrollPercent = (s / (c)) * 20;
-            var position = scrollPercent/50;
+        d = $(document).height(),
+        c = $(window).height();
+    scrollPercent = (s / (c)) * 20;
+    var position = scrollPercent / 50;
     $("#titlebar").attr('style', "background: rgba(0, 0, 0, " + position + ");");
     $("#navi").attr('style', "background: rgba(0, 0, 0, " + position + ");");
     $("#logo").attr('style', "opacity: " + position + ";");
 }
 
 // Sets the navigation default to black so it can be more easily interacted with
-function navDefault(){
+function navDefault() {
     if ($(window).width() < 950) {
         $("#titlebar").attr('style', "background: rgba(0, 0, 0, 1);");
         $("#navi").attr('style', "background: rgba(0, 0, 0, 1);");
@@ -82,22 +83,111 @@ window.onresize = function(event) {
 };
 
 // Sets the navigation bar background colour
-$(window).scroll(function () {
+$(window).scroll(function() {
     if ($(window).width() > 950) {
         scrollColour();
+
+
+
     }
 });
 
 // Changes nav bar focus on click
-$("a").click(function(){
+$("a").click(function() {
     x = $(this).data("value");
-    if (x !== undefined){
+    if (x !== undefined) {
         state = Number(x);
         switchFocus(state);
         setCookie("nav", state, "7");
     }
 });
 
+
+function createTestimonialCarousel() {
+    if ($(window).width() > 950) {
+        $(document).ready(function() {
+            $('.testimonials-carousel').slick({
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                arrows: true,
+                autoplay: true,
+                mobileFirst: true,
+                swipe: true,
+            });
+
+        });
+    } else {
+        $(document).ready(function() {
+            $('.testimonials-carousel').slick({
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: true,
+                mobileFirst: true,
+                swipe: true,
+            });
+
+        });
+    }
+}
+
+
+// $(document).ready(function() {
+//     $('.testimonials-carousel').slick({
+//         infinite: true,
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         arrows: true,
+//         autoplay: true,
+//         mobileFirst: true,
+//         swipe: true,
+//     });
+
+// });
+
+
+
+// var TESTIMONIALS = document.querySelectorAll(".testimonials-card");
+// console.log(TESTIMONIALS)
+// var displayedTestimonials = [0, 1, 2];
+
+// function setTestimonials() {
+//     for (var i = 0; i < TESTIMONIALS.length; i++) {
+//         if (i === displayedTestimonials[0] || i === displayedTestimonials[1] || i === displayedTestimonials[2]) {} else {
+//             TESTIMONIALS[i].classList.add("hide");
+//         }
+
+//     }
+//     for (var i = 0; i < displayedTestimonials.length; i++) {
+//         TESTIMONIALS[displayedTestimonials[i]].classList.remove("hide");
+//         TESTIMONIALS[displayedTestimonials[i]].classList.add("show-testimonial");
+//     }
+
+
+// }
+
+// function testimonialsLeft() {
+//     for (var i = 0; i < 3; i++) {
+//         displayedTestimonials[i] = ((displayedTestimonials[i] + 1) == TESTIMONIALS.length) ? 0 : (displayedTestimonials[i] + 1);
+//     }
+//     setTestimonials();
+//     console.log(displayedTestimonials)
+// }
+
+// function testimonialsRight() {
+//     for (var i = 0; i < 3; i++) {
+//         displayedTestimonials[i] = ((displayedTestimonials[i] - 1) < 0) ? (TESTIMONIALS.length - 1) : (displayedTestimonials[i] - 1);
+//     }
+//     setTestimonials();
+//     console.log(displayedTestimonials)
+// }
+
+// document.getElementById("testimonials-left").addEventListener("click", testimonialsLeft);
+// document.getElementById("testimonials-right").addEventListener("click", testimonialsRight);
+
+
+
 // ___Main___
 var state;
-__init__();  
+__init__();
